3070f4000a14713b4965a18b653acb2ea733c177
Danys Linares <danlinmoy@gmail.com>
2023-07-11 12:19:29 -0400

    Delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
+  root "dashboard#index"
   resources :products
-  get 'dashboard/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-   root "dashboard#index"
 end
